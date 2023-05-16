b3ecec0e780694b867cdbaa386f09278811e8b6c
Tsumbedzo_Nevhutalu <vanessatn96@gmail.com>
2023-05-16 15:54:26 +0200

    added the roation animation to the splash screen and added the color pallet

--- LoginSubsystem/Frontend/UserLogin/app/src/main/java/com/example/userlogin/MainActivity.kt
+++ LoginSubsystem/Frontend/UserLogin/app/src/main/java/com/example/userlogin/MainActivity.kt
@@ -3,14 +3,23 @@ package com.example.userlogin
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 
 private lateinit var tvR: TextView
 
+
+
+
 class MainActivity : AppCompatActivity() {
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tvR
 
         tvR = findViewById(R.id.tvRoute)
 
--- LoginSubsystem/Frontend/UserLogin/app/src/main/java/com/example/userlogin/SplashScreenActivity.kt
+++ LoginSubsystem/Frontend/UserLogin/app/src/main/java/com/example/userlogin/SplashScreenActivity.kt
@@ -1,18 +1,39 @@
 package com.example.userlogin
 
+
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.ImageView
 
 private lateinit var ivloginIcon: ImageView
 
 class SplashScreenActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        ivloginIcon = findViewById(R.id.iv_loginIcon)
+//added rotation to the splashscreen
+
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN )
+        val backgroundImage: ImageView = findViewById(R.id.iv_loginIcon)
+        val slideAnimation = AnimationUtils.loadAnimation(this, R.anim.rotate)
+        backgroundImage.startAnimation(slideAnimation)
+        Handler().postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()}, 3000)
+
+
+
+        ivloginIcon= findViewById(R.id.iv_loginIcon)
 
         ivloginIcon.alpha =0f
         ivloginIcon.animate().setDuration(2000).alpha(1f).withEndAction {
@@ -22,4 +43,6 @@ class SplashScreenActivity : AppCompatActivity() {
             finish()
         }
     }
-}
+
+
+    }
